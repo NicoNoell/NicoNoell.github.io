f567688d7f6147ad0e1b5830852ad59e9cc1794f
nico.noell <nico.noell@advancis.de>
2024-07-16 15:02:51 +0200

    Better Scrolling

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@ var txt = 'And welcome to my Website :)';
 var speed = 50;
 
 window.addEventListener('scroll', () => {
-    document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-  }, false);
+    setSectionPositions(window.scrollY);
+}, false);
 
 function onPageLoad() {
     setTimeout(typeWriter, 1800)
@@ -15,7 +15,18 @@ function onPageLoad() {
 function typeWriter() {
     if (i < txt.length) {
       document.getElementById("WelcomeToWebsite").innerHTML += txt.charAt(i);
+      const s1 = document.getElementById("Section1");
+      //s1.style.transform = 'translateY(' + i * 5 + 'px)';
       i++;
       setTimeout(typeWriter, speed);
     }
   }
+
+function setSectionPositions(scroll) {
+    const s1 = document.getElementById("Section1");
+    s1.style.transform = 'translateY(' + scroll * -1 + 'px)';
+    const s2 = document.getElementById("Section2");
+    s2.style.transform = 'translateY(' + scroll * -2 + 'px)';
+    const s3 = document.getElementById("Section3");
+    s3.style.transform = 'translateY(' + scroll * -3 + 'px)';
+}
