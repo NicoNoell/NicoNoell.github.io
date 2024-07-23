6406f6d7ed904b522994d60147953850df42d00a
nico.noell <nico.noell@advancis.de>
2024-07-23 12:56:30 +0200

    Alternate color test

--- projects/projectsScripts.js
+++ projects/projectsScripts.js
@@ -7,14 +7,16 @@ function setupCollapsibleContent(){
 
     for (var i = 0; i < coll.length; i++) {
         coll[i].addEventListener("click", function() {
-          this.classList.toggle("active");
           var content = this.previousElementSibling;
           if (content.style.maxHeight) {
             content.style.maxHeight = null;
             this.innerText = "⮮ Continue reading ⮯";
+            this.classList.remove("active");
           } else {
             content.style.maxHeight = content.scrollHeight + "px";
             this.innerText = "⮬ Minimize content ⮭";
+            this.classList.active = true;
+            this.classList.add("active");
           }
         });
       }
